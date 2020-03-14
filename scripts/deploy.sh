ca3016ff3b16c9b6b7141667e54f4f01c010ee7c
Alexander Teslovskiy <artoriousso@gmail.com>
2020-03-14 22:06:36 +0300

    Add key file variable

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+# execute command on server
 execute() {
-  ssh -i squadster.pem $INSTANCE_USER@$INSTANCE_IP -T $@
+  ssh -i $INSTANCE_KEY_FILE $INSTANCE_USER@$INSTANCE_IP -T $@
 }
 
+# send file to app directory
 transfer() {
-  scp -r -i squadster.pem $1 $INSTANCE_USER@$INSTANCE_IP:$2
+  scp -r -i $INSTANCE_KEY_FILE $1 $INSTANCE_USER@$INSTANCE_IP:$2
 }
 
 source .env
@@ -18,9 +20,10 @@ transfer docker-compose.yml $app_dir
 transfer .env $app_dir
 transfer nginx $app_dir
 
+# install docker if not installed (semicolons required)
 execute "
   if [[ ! \$(which docker | grep -o docker) ]]; then
-    echo \"No docker found installing...\"
+    echo \"No docker found installing...\";
 
     sudo apt-get update;
     sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common;
@@ -28,13 +31,14 @@ execute "
     sudo add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/ubuntu \$(lsb_release -cs) stable\";
     sudo apt-get update -y;
     sudo apt-get install -y docker-ce docker-ce-cli containerd.io;
-    sudo usermod -aG docker $INSTANCE_USER
+    sudo usermod -aG docker $INSTANCE_USER;
   fi
 "
 
+# install docker-compose if not installed
 execute "
   if [[ ! \$(which docker-compose | grep -o docker-compose) ]]; then
-    echo \"No docker-compose found installing...\"
+    echo \"No docker-compose found installing...\";
 
     sudo curl -L \"https://github.com/docker/compose/releases/download/1.25.4/docker-compose-\$(uname -s)-\$(uname -m)\" -o /usr/local/bin/docker-compose;
     sudo chmod +x /usr/local/bin/docker-compose;
