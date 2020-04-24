4fa34964264aa3362d059b959e65a91118d94a55
Alexander Teslovskiy <artoriousso@gmail.com>
2020-04-25 00:18:30 +0300

    Remove old images on redeploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -48,4 +48,4 @@ execute "
 "
 
 execute "echo $GITHUB_TOKEN | docker login -u $GITHUB_USER --password-stdin docker.pkg.github.com"
-execute "cd $app_dir && docker-compose down && docker-compose build --parallel --pull && docker-compose up -d --force-recreate"
+execute "cd $app_dir && docker-compose down --rmi all && docker-compose build --parallel --pull && docker-compose up -d --force-recreate"
