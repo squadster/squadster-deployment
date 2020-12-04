09316e0b9085d1e1fb625629d450230e28afffa6
Alexander Teslovskiy <artoriousso@gmail.com>
2020-12-04 19:41:16 +0300

    Add initial script for docker-machine setup

---
+++ scripts/create_machine.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script adds the docker-machine host
+# Host machine setup guide: https://thobalose.co.za/2017/add-existing-host-docker-machine/#
+
+source .env
+
+docker-machine create --driver generic \
+  --generic-ip-address $INSTANCE_IP \
+  --generic-ssh-user $INSTANCE_USER \
+  --generic-ssh-key ~/.ssh/id_rsa \
+  $INSTANCE_NAME
