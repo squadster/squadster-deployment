0e8aefbb7c38ad7d7853946a0333fdaca530fe8a
Alexander Teslovskiy <artoriousso@gmail.com>
2020-12-13 22:59:53 +0300

    Fix build script

--- bin/build_nginx.sh
+++ bin/build_nginx.sh
@@ -2,9 +2,11 @@
 
 # Rebuild nginx image and push it to the registry
 
-source .env
+if test -f ".env"; then
+  source .env
+fi
 
 echo $GITHUB_TOKEN | docker login -u $GITHUB_USER --password-stdin docker.pkg.github.com
 
 docker build -t docker.pkg.github.com/squadster/squadster-deployment/nginx:$RELEASE_VERSION nginx
-docker push docker.pkg.github.com/squadster/squadster-api/release:$RELEASE_VERSION
+docker push docker.pkg.github.com/squadster/squadster-deployment/nginx:$RELEASE_VERSION
